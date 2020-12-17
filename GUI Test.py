ec3c9a93efe76e694c63e98b086b1fa335d8b9cd
8bitfishie <sowerbutts.pg@hotmail.com>
2020-12-17 12:26:59 +0000

    Resizing (somewhat) fixed

--- GUI Test.py
+++ GUI Test.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.font as font
 
 top = Tk()
 screen_width = top.winfo_screenwidth()
@@ -6,7 +7,10 @@ screen_height = top.winfo_screenheight()
 intital_window_width = screen_width/2
 intital_window_height = screen_height/2
 top.geometry(f"{int(intital_window_width)}x{int(intital_window_height)}")
+print(f"screen = {screen_width} x {screen_height}")
 
+print(f"window = {intital_window_width} x {intital_window_height}")
+buttonfont = font.Font(size=16)
 
 class App:
     def __init__(self):
@@ -22,29 +26,53 @@ class App:
         self.buttonlist = Frame(self.buttoncanvas, bg="#aeaeae")
         self.infotab = Frame(top, bg="#aeaeae")
 
-        self.btn1 = Button(self.buttonlist, text="Device 1", command=lambda: self.button1press())
-        self.btn2 = Button(self.buttonlist, text="Device 2")
-        self.btn3 = Button(self.buttonlist, text="Device 3")
-        self.btn4 = Button(self.buttonlist, text="Device 4")
-        self.btn5 = Button(self.buttonlist, text="Device 5")
-        self.btn6 = Button(self.buttonlist, text="Device 6")
-        self.btn7 = Button(self.buttonlist, text="Device 7")
+        self.btn1 = Button(self.buttonlist, text="Device 1", font = buttonfont, command=lambda: self.button1press())
+        self.btn2 = Button(self.buttonlist, font = buttonfont, text="Device 2")
+        self.btn3 = Button(self.buttonlist, font = buttonfont, text="Device 3")
+        self.btn4 = Button(self.buttonlist, font = buttonfont, text="Device 4")
+        self.btn5 = Button(self.buttonlist, font = buttonfont, text="Device 5")
+        self.btn6 = Button(self.buttonlist, font = buttonfont, text="Device 6")
+        self.btn7 = Button(self.buttonlist, font = buttonfont, text="Device 7")
 
         self.scrollbar.config(command=self.buttoncanvas.yview)
         self.buttoncanvas.config(yscrollcommand = self.scrollbar.set)
 
     def _canvas_resize(self, event):
+        print(f"window_width = {self.window_width}")
+        print(f"event width = {event.width}")
+
         self.window_width = event.width/2
         self.buttoncanvas.itemconfigure("text", width=self.window_width)
         self.buttoncanvas_width = self.window_width
-        self.calldefault()
 
+        print(f"buttoncanvas update = {self.buttoncanvas_width} x {self.window_height/5}")
+        self.calldefault()
+        return
 
     def scroll(self, event):
         self.buttoncanvas.configure(scrollregion=self.buttoncanvas.bbox("all"))
 
+
     def calldefault(self):
-        self.topframe.pack(expand=TRUE, fill=BOTH)
+
+        if self.display_info_tab:
+            self.topframe.pack(side=LEFT, expand=TRUE, fill=BOTH)
+            self.infotab.pack(side=RIGHT, expand=TRUE, fill=BOTH)
+            self.slider = Scale(self.infotab, sliderlength=self.infotab.winfo_height() / 4,
+                                width=self.infotab.winfo_width(),
+                                showvalue=0, command=self.update_slider1_val)
+
+            self.slider.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+            self.slider.set(self.slider1_val)
+
+        else:
+            self.topframe.pack(expand=TRUE, fill=BOTH)
+            self.infotab.pack_forget()
+
+
+        print(f"buttoncanvas = {self.buttoncanvas_width} x {self.window_height/5}")
+
         self.buttoncanvas.pack(side=LEFT, expand=TRUE, fill=BOTH)
         self.scrollbar.pack(side=RIGHT, expand=FALSE, fill=Y, ipadx=10)
         self.buttonlist.pack(side=TOP, expand=TRUE, fill=BOTH)
@@ -60,39 +88,24 @@ class App:
         self.buttoncanvas.create_window((0, 0), window=self.buttonlist, anchor='nw')
         self.topframe.bind("<Configure>", self.scroll)
         self.buttoncanvas.bind("<Configure>", self._canvas_resize)
+        return
 
     def button1press(self):
         if self.display_info_tab == 1:
-            self.infotab.destroy()
-            self.calldefault()
             self.display_info_tab = 0
+            self.calldefault()
 
         else:
             self.display_info_tab = 1
             self.btn1.config(bg="#bfbfbf")
-            self.resizebuttonlist()
+            self.calldefault()
         return
 
-    def printscaleval(self):
-        print(str(self.slider.get()))
-        self.slider1_val = self.slider.get()
+    def update_slider1_val(self, val):
+        print(val)
+        self.slider1_val = val
         return
 
-    def resizebuttonlist(self):
-        self.infotab = Frame(top, bg="#aeaeae")
-        self.topframe.pack(side=LEFT, expand=TRUE, fill=BOTH)
-        self.infotab.pack(side=RIGHT, expand=TRUE, fill=BOTH)
-        top.update_idletasks()
-        self.slider = Scale(self.infotab, sliderlength=self.infotab.winfo_height() / 4, width=self.infotab.winfo_width(),
-                       showvalue=0, variable=self.slider1_val)
-
-        self.applybutton = Button(self.infotab, text="Apply", command=lambda: self.printscaleval())
-
-        self.slider.place(relx=0, rely=0, relwidth=1, relheight=0.9)
-        self.applybutton.place(relx=0, rely=0.9, relwidth=1, relheight=0.1)
-
-        self.slider.set(self.slider1_val)
-        return
 
 
 app = App()
